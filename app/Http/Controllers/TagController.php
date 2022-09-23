22be7c6cb57b4285393b79842e246d8ef0ea52e6
Mikhael <mikhaelkevinlubis@gmail.com>
2022-09-23 20:09:19 +0700

    add(tag): tag features.
    create tag features following:
        #get tags.
        #get tag.
        #create tag.
        #update tag.
        #delete tag.
    
    above features is private routes.
    
    add tag-features.

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    public function getTags()
+    {
+        $tags = Tag::all();
+        return response()->json($tags);
+    }
+
+    public function getTag($tagId)
+    {
+        $tag = Tag::find($tagId);
+        return response()->json($tag);
+    }
+
+    public function createTag(Request $req)
+    {
+        $validated = $this->validate($req, [
+            'tagName' => 'required|unique: tags, tag_name|min:3'
+        ]);
+
+        $tag = new Tag();
+        $tag->tag_name = $validated['tagName'];
+        $tag->save();
+
+        return response()->json(['message' => 'Tag has been added!']);
+    }
+    public function editTag(Request $req, $tagId)
+    {
+        $tag = Tag::find($tagId);
+        if (!$tag) return response()->json(['message' => 'Cannot found spesific tag!'], 404);
+
+        $tempTagName = $req->tagName ? $req->tagName : $tag->tag_name;
+
+        $tag->update([
+            'tag_name' => $tempTagName
+        ]);
+
+        return response()->json(['message' => 'Tag has been updated!']);
+    }
+    public function deleteTag($tagId)
+    {
+        $tag = Tag::find($tagId);
+        if (!$tag) return response()->json(['message' => 'Cannot found spesific tag!'], 404);
+
+        $tag->delete();
+        return response()->json(['message' => 'Tag has been deleted!']);
+    }
+}
